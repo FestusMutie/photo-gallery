f7caae1d7458cc252b5a47cbde439113f2d23516
FestusMutie <mutiefestus84@gmail.com>
2021-07-05 10:24:43 +0300

    create the welcome view function

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,
+from django.http  import HttpResponse
 
 # Create your views here.
+# view is a function that takes the request from a user, processes it and returns a response to the user.
+
+
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Moringa Gallery')
